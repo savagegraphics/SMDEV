3e5917e77d111022f636c4c2ccbd4e5fcf23352b
olawaleraji <savagedotjsx@gmail.com>
2024-05-28 09:18:33 -0700

    first commit

--- app/FeatureCard.jsx
+++ app/FeatureCard.jsx
@@ -7,19 +7,19 @@ const FeatureCard = () => {
         <div className='mb-10 lg:mb-16 flex justify-center items-center flex-col gap-x-0 gap-y-6 lg:gap-y-0 lg:flex-row lg:justify-between max-md:max-w-lg max-md:mx-auto'>
           <div className='relative w-full text-center lg:text-left lg:w-2/4'>
             <h2 className='text-4xl font-bold text-gray-900 leading-[3.25rem] lg:mb-6 mx-auto max-w-max lg:max-w-md lg:mx-0'>
-              Enjoy the finest features with our products
+              Unlock the Power of MERN Stack Development
             </h2>
           </div>
           <div className='relative w-full text-center lg:text-left lg:w-2/4'>
             <p className='text-lg font-normal text-gray-500 mb-5'>
-              We provide all the advantages that can simplify all your financial
-              transactions without any further requirements.
+              Bringing top-notch web applications to life with MongoDB,
+              Express.js, React, and Node.js.
             </p>
             <a
               href='#'
               className='flex flex-row items-center justify-center gap-2 text-base font-semibold text-indigo-600 lg:justify-start hover:text-indigo-700'
             >
-              Button CTA
+              Discover My Work
               <svg
                 width='20'
                 height='20'
@@ -49,18 +49,20 @@ const FeatureCard = () => {
                 xmlns='http://www.w3.org/2000/svg'
               >
                 <path
-                  d='M24.7222 11.6667V7.22225C24.7222 5.99495 23.7273 5 22.5 5H4.72222C3.49492 5 2.5 5.99492 2.5 7.22222V22.7778C2.5 24.0051 3.49492 25 4.72222 25H22.5C23.7273 25 24.7222 24.005 24.7222 22.7777V17.7778M20.8333 17.7778H25.2778C26.5051 17.7778 27.5 16.7829 27.5 15.5556V13.8889C27.5 12.6616 26.5051 11.6667 25.2778 11.6667H20.8333C19.606 11.6667 18.6111 12.6616 18.6111 13.8889V15.5556C18.6111 16.7829 19.606 17.7778 20.8333 17.7778Z'
+                  d='M20 10L15 15L10 10'
                   stroke='#4F46E5'
                   stroke-width='2'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
                 ></path>
               </svg>
             </div>
             <h4 className='text-xl font-semibold text-gray-900 mb-3 capitalize transition-all duration-500 group-hover:text-white'>
-              Easy Payment
+              Backend Development
             </h4>
             <p className='text-sm font-normal text-gray-500 transition-all duration-500 leading-5 group-hover:text-white'>
-              We provide various methods for you to carry out all transactions
-              related to your finances.
+              Crafting scalable and secure backend solutions using Node.js and
+              Express.js.
             </p>
           </div>
           <div className='group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-indigo-600'>
@@ -73,7 +75,7 @@ const FeatureCard = () => {
                 xmlns='http://www.w3.org/2000/svg'
               >
                 <path
-                  d='M14.375 15.8571C16.1009 15.8571 17.5 14.458 17.5 12.7321C17.5 11.0062 16.1009 9.6071 14.375 9.6071C12.6491 9.6071 11.25 11.0062 11.25 12.7321C11.25 14.458 12.6491 15.8571 14.375 15.8571ZM14.375 15.8571V20.8571M3.75 13.2264V15.2343C3.75 17.6868 3.75 18.9131 4.27747 19.9685C4.80493 21.0239 5.78567 21.76 7.74715 23.2322L8.57248 23.8516C11.4626 26.0208 12.9077 27.1054 14.5753 27.1054C16.243 27.1054 17.688 26.0208 20.5782 23.8516L21.4035 23.2322C23.365 21.76 24.3457 21.0239 24.8732 19.9685C25.4006 18.9131 25.4006 17.6868 25.4006 15.2343V13.2264C25.4006 9.95932 25.4006 8.32576 24.546 7.05852C23.6913 5.79128 22.1768 5.17918 19.1477 3.95499L18.3223 3.62144C16.4724 2.87381 15.5475 2.5 14.5753 2.5C13.6032 2.5 12.6782 2.87381 10.8283 3.62144L10.003 3.95499C6.97389 5.17919 5.45934 5.79128 4.60467 7.05852C3.75 8.32576 3.75 9.95932 3.75 13.2264Z'
+                  d='M15 10L20 15L15 20L10 15L15 10Z'
                   stroke='#4F46E5'
                   stroke-width='2'
                   stroke-linecap='round'
@@ -82,11 +84,10 @@ const FeatureCard = () => {
               </svg>
             </div>
             <h4 className='text-xl font-semibold text-gray-900 mb-3 capitalize transition-all duration-500 group-hover:text-white'>
-              Safe Transaction
+              Frontend Development
             </h4>
             <p className='text-sm font-normal text-gray-500 transition-all duration-500 leading-5 group-hover:text-white'>
-              We have the most up-to-date security to support the security of
-              all our customers in carrying out all transactions.
+              Building dynamic and responsive user interfaces with React.js.
             </p>
           </div>
           <div className='group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-indigo-600'>
@@ -99,7 +100,7 @@ const FeatureCard = () => {
                 xmlns='http://www.w3.org/2000/svg'
               >
                 <path
-                  d='M20.8333 16.25H24.1667C24.7384 16.25 25.2503 16.0127 25.6136 15.6145C25.9768 15.2163 26.154 14.6942 26.1042 14.1667L25.3925 6.77706C25.3207 5.96277 24.6306 5.3125 23.8114 5.3125H6.18857C5.3694 5.3125 4.67931 5.96277 4.60753 6.77706L3.8958 14.1667C3.84604 14.6942 4.0232 15.2163 4.38645 15.6145C4.74971 16.0127 5.26156 16.25 5.83333 16.25H9.16667M12.0833 19.1667H17.9167C18.8027 19.1667 19.5286 19.8926 19.5286 20.7786V23.0556C19.5286 24.7912 19.5286 25.6589 19.1243 26.2783C18.72 26.8977 18.1243 27.0833 16.9328 27.0833H13.0672C11.8757 27.0833 11.28 26.8977 10.8757 26.2783C10.4714 25.6589 10.4714 24.7912 10.4714 23.0556V20.7786C10.4714 19.8926 11.1973 19.1667 12.0833 19.1667Z'
+                  d='M15 10L20 20H10L15 10Z'
                   stroke='#4F46E5'
                   stroke-width='2'
                   stroke-linecap='round'
@@ -108,11 +109,11 @@ const FeatureCard = () => {
               </svg>
             </div>
             <h4 className='text-xl font-semibold text-gray-900 mb-3 capitalize transition-all duration-500 group-hover:text-white'>
-              Secure Vault
+              Database Management
             </h4>
             <p className='text-sm font-normal text-gray-500 transition-all duration-500 leading-5 group-hover:text-white'>
-              All your valuable assets are stored securely with the best
-              protection methods from us.
+              Managing and optimizing databases with MongoDB for seamless data
+              flow.
             </p>
           </div>
         </div>
