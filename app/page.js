10371bc5f8d05f63e798c2a6042379293cc27aa2
olawaleraji <savagedotjsx@gmail.com>
2024-05-29 03:26:14 -0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import BackgroundImage from './BackgroundImage';
+import Footer from './Footer';
 import Nav from './Nav'
 
 export default function Home() {
   return (
     <BackgroundImage>
-    <Nav/>
+    {/* <Nav/> */}
+    <Footer/>
     </BackgroundImage>
   );
 }
